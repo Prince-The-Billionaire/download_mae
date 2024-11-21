81849e6cbd07226347e7e57ee3823a5edd398684
Prince-The-Billionaire <danielprincewill14@gmail.com>
2024-11-21 10:16:08 +0100

    fourth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Instructions from '@/components/Instructions'
 
 const page = () => {
   return (
@@ -13,7 +14,7 @@ const page = () => {
       />
       <h1 className='text-4xl font-sans'>Download Moodle Auto Enroller</h1>
       <a href='./M.A.E.zip' download className='px-3 py-2 w-fit bg-blue-500 border-none rounded-md cursor-pointer no-underline'>Download Now</a>
-      
+      <Instructions/>
     </div>
   )
 }
---
+++ components/Instructions.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Instructions = () => {
+  return (
+    <div>
+        <video src='/moodle.mp4' muted autoPlay className='m-10 rounded-lg aspect-video w-[700px]  ' />
+        <div className='mt-16'>
+            <h1>How to use M.A.E when you've enrolled all your course</h1>
+            <p>Download the extension on any browser of your choice</p>
+            <p>Dm @admiral_prince_daniel on telegram to get your token </p>
+            <p>Fill in your details on the extension and watch the extension do it's magic</p>
+            <p>It should open moodle by itself and take you to the dashboard</p>
+            <p>click the course you want to evaluate and watch it do it in seconds or... you could just use that time to do something else</p>
+            <p>Do it for all your courses</p>
+        </div>
+    </div>
+  )
+}
+
+export default Instructions
